4686816611e2c7bf8b604dafec35c13acc16094a
Harsh V Srivastava <harshvsri@gmail.com>
2023-12-14 15:11:05 +0530

    JS inject in footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+const socials = ["twitter", "facebook", "instagram"];
+const icons = document.querySelector(".icons");
+console.log(icons);
+socials.forEach((social) => {
+  const icon = document.createElement("i");
+  icon.className = `footer-icon fa-brands fa-${social} fa-2x`;
+  icons.appendChild(icon);
+});
+
 const copyright = document.querySelector(".copyright");
 const currYear = new Date().getFullYear();
 copyright.innerHTML = `© Copyright ${currYear} TinDog & Harsh`;
